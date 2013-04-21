6ea1eeecfa2e47e7f351553d4af036c802654b34
Peter Concha Regatto <peter.concha@outlook.com>
2013-04-21 16:14:07 -0500

    Create breadcrumbs.php
    Con esto desarrollamos un migajas para marcar la posición en la que se encuentra el cliente mientras navega esto lo realizamos con funciones nativas
    
    Se lo emplea de la siguiente manera
    
    <!DOCTYPE html>
    <html lang="en">
    <head>
        <meta charset="UTF-8">
        <title></title>
    </head>
    <?php include "breadcrumbs.php"; ?>
    <body>
        <?= breadcrumbs();?>
    </body>
    </html>

---
+++ breadcrumbs.php
@@ -0,0 +1,52 @@
+<?php
+/*
+  Inicializamos la funcion breadcrumbs() con dos variables fijas que serian $separator la cual asignara el separador 
+  de cada fase del breadcrumbs, en este caso es el simbolo '>>' escrito en formato ascii $home que sera el reemplazo 
+  a la variable raiz del sistema.
+*/
+function breadcrumbs($separator = ' ? ', $home = 'Inicio') 
+{
+  /*
+    $path parseara o "cortara" las cadenas de palabras cuando encuentre un '/' para formar las rutas de nuestro 
+    breadcrumbs.
+  */  
+    $path = array_filter(explode('/', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)));
+  //$base sera la encargada de rastrear nuestra raiz ya sea http o https.
+    $base = ($_SERVER['HTTPS'] ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/';
+  /*
+    $breadcrumbs creara el primer link en este caso sera nuestra variable $home, $last es quien tomara la ultima 
+    variable de nuestro array.
+  */
+    $breadcrumbs = array('<a href="'. $base .'">'. $home .'</a>');
+ 
+    $last = end(array_keys($path));
+  /*
+    Generamos un ciclo, de esta forma recorrera el array completo generado anteriormente, tomando las 'key' de los 
+    array es decir las palabras claves no el contenido, sacamos las extension (en este caso remplaza las extensiones
+    php y _) creado el breadcrumbs.
+  */
+    foreach ($path as $x => $crumb) {
+        $title = ucwords(str_replace(array('.php', '_'), array('', ' '), $crumb));
+ 
+        if ($x != $last) {
+            $breadcrumbs[] = '<a href="'. $base . $crumb .'">'. $title .'</a>";
+        } else {
+            $breadcrumbs[] = $title;
+        }
+    }
+ 
+    return implode($separator, $breadcrumbs);
+}
+?>   
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<?php include "breadcrumbs.php"; ?>
+<body>
+    <?= breadcrumbs();?>
+</body>
+</html>    
